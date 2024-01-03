96dc300b52f267f623c1740a4d2d6545c52f4994
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-03 08:13:55 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springbootauthserver/SpringBootAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springbootauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAuthServerApplication.class, args);
+    }
+
+}
